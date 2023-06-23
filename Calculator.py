9a19440a29b561dff041dd9b513b3739f7d0b680
mohammad <mohammadhosseini4621@gmail.com>
2023-06-23 13:14:45 -0700

    Fourth Step = Added new if for Minus
    Added new if for Minus, When user enter your operator, User_Operator go to if for Minus

--- Calculator.py
+++ Calculator.py
@@ -7,3 +7,9 @@ if User_Operator == "+":
     Plus_Second_Number = int(input("Enter your Second Number (Plus +) --> "))
     Plus_SUM = Plus_First_Number + Plus_Second_Number
     print("Plus Result --> " , Plus_SUM)
+
+if User_Operator == "-":
+    Minus_First_Number = int(input("Enter your First Number (Minus -) --> "))
+    Minus_Second_Number = int(input("Enter your Second Number (Minus -) --> "))
+    Minus_SUM = Minus_First_Number - Minus_Second_Number
+    print("Minus Result --> " , Minus_SUM)

a5c68ce3b5a90800c1cb4c0545f5c6fc5aec78e2
mohammad <mohammadhosseini4621@gmail.com>
2023-06-23 13:11:08 -0700

    Third Step = Added new if for Plus
    Added new if for Plus, When user enter your operator, User_Operator go to if for Plus

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,9 @@
 #Create Simple Calculator With Python
 
 User_Operator = input("Enter your Operator (+, -, *, /) --> ")
+
+if User_Operator == "+":
+    Plus_First_Number = int(input("Enter your First Number (Plus +) --> "))
+    Plus_Second_Number = int(input("Enter your Second Number (Plus +) --> "))
+    Plus_SUM = Plus_First_Number + Plus_Second_Number
+    print("Plus Result --> " , Plus_SUM)
